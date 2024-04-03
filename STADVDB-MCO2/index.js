f2c133fac79eaee37692e5465dee228a2c805da2
Geoffrey122 <114456221+Geoffrey122@users.noreply.github.com>
2024-04-03 16:29:02 +0800

    Some more designs YEAH
    - added navbar to all pages
    - make html files to ejs
    - added updateAppointments design
    - added clinicid / virtualind columns in viewSearch

--- STADVDB-MCO2/index.js
+++ STADVDB-MCO2/index.js
@@ -9,8 +9,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/images', express.static(path.join(__dirname, 'views', 'images'))); // newly added to be able to use the logo
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
 app.use('/', routes)
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');

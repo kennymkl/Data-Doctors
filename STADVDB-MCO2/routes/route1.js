63d10fccd6d23e8ce96d232cc9aec27aff59965e
RojiTech <kennmichael.villarama@gmail.com>
2024-04-10 19:35:33 +0800

    ccs cloud deployment details

--- STADVDB-MCO2/routes/route1.js
+++ STADVDB-MCO2/routes/route1.js
@@ -6,16 +6,16 @@ const mysql = require('mysql');
 
 // MASTER
 const masterConfig = {
-    host: 'localhost',
-    user: 'root',
+    host: '10.2.0.81',
+    user: 'group24',
 
-    password: 'melgeoffrey', //change password to specific credentials
+    password: 'databeasts', //change password to specific credentials
 
     database: 'mco2',
   };
                             //when updating in the vms, add a host:  
-const slave1Config = { ...masterConfig, database: 'mco2slave1'}; //database names
-const slave2Config = { ...masterConfig, database: 'mco2slave2'}; //database names
+const slave1Config = { ...masterConfig, host:'10.2.0.82', database: 'mco2slave1'}; //database names
+const slave2Config = { ...masterConfig, host:'10.2.0.83', database: 'mco2slave2'}; //database names
 
 var db = createConnection(masterConfig,'master');
 var db_slave1 = createConnection(slave1Config,'slave1');
